b93065a662cf645e346d11ca94e38f4261e22b98
Prem Raichura <premraichura7@gmail.com>
2025-11-01 17:50:46 +0530

    Updated the About page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
               initial={{ y: -40, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-              Vehicle Dencity & Detection
+              Vehicle Density & Detection
             </motion.h1>
             <p className="text-lg text-gray-700 mb-10 z-10">
               AI-powered YOLO-based traffic analysis
